9a55f25f2c19cfd2bd7648d7f13df38ab1564aad
latelsie <tonaelsie31@gmail.com>
2024-10-25 13:50:54 +0200

    exercises

--- src/componts/Hero.jsx
+++ src/componts/Hero.jsx
@@ -14,7 +14,7 @@ const Hero =()=>{
 
         <div className='w-full lg:w-1/2'>
             <div className='flex flex-col items-center lg:items-start mt-10'>
-                <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Tona Elsie</h2>
+                <h2 className='pb-2 text-sm tracking-tighter lg:text-8xl'>Tona Elsie</h2>
                 <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>
                     Front-end Developer
                 </span>
--- src/componts/navbar.jsx
+++ src/componts/navbar.jsx
@@ -1,36 +1,24 @@
 import { FaGithub, FaInbox, FaLinkedin, FaMailBulk, FaMailchimp, FaTwitter } from 'react-icons/fa';
 import logo from '../assets/elsielogo.webp';
 
-const Navbar =()=>{
-   return(
-   <nav className="flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-    <a href="/" aria-label="Home">
-    <img src={logo} className="mx-3 w-20 h-auto" alt="Logo" />
-</a>
-
-
-
-    </div>
-    <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <a href='https://www.linkedin.com/in/tona-elsie-230b68271' target='_blank' 
-        rel='noopener noreferrer' aria-label='Linkedin'>
-            <FaLinkedin/>
-        </a>
-        <a href='https://github.com/latelsie' target='_blank' 
-        rel='noopener noreferrer' aria-label='Github'>
-            <FaGithub/>
-        </a>
-        <a href='https://x.com/home' target='_blank' 
-        rel='noopener noreferrer' aria-label='Twitter'>
-            <FaTwitter/>
-        </a>
-        <a href='#' target='_blank' 
-        rel='noopener noreferrer' aria-label='Mailbulk'>
-            <FaMailBulk/>
-        </a>
-    </div>
-   </nav>
-   )
-}
-export default Navbar;
+const Navbar = () => {
+    return (
+      <nav className="flex items-center justify-between py-6 px-4">
+        <div className="flex flex-shrink-0 items-center">
+          <a href="/" aria-label="Home">
+            <img src={logo} className="mx-3 w-20 h-auto" alt="Logo" />
+          </a>
+        </div>
+        <div className="flex items-center justify-center gap-6 text-lg mx-auto"> {/* Centering with mx-auto */}
+          <a href="#hero" className="hover:text-stone-300" aria-label="Home">Home</a>
+          <a href="#technologies" className="hover:text-stone-300" aria-label="Technologies">Technologies</a>
+          <a href="#projects" className="hover:text-stone-300" aria-label="Projects">Projects</a>
+          <a href="#experience" className="hover:text-stone-300" aria-label="Experience">Experience</a>
+          <a href="#contact" className="hover:text-stone-300" aria-label="Contact Us">Contact Us</a>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
